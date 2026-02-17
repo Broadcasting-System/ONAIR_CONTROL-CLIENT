91c8bd854780c76c0aba550271973a21bf66a4db
CraftsManShip001 <seoj080829@gmail.com>
2026-02-18 08:52:01 +0900

    feat :: 상수 데이터 추가

---
+++ src/constants/navigation.ts
@@ -0,0 +1,8 @@
+export const NAVIGATION_ITEMS = [
+  { label: "메인 세팅", subLabel: "MAIN", path: "/main" },
+  { label: "시보 설정", subLabel: "TIME", path: "/time" },
+  { label: "오디오 믹스", subLabel: "AUDIO", path: "/audio" },
+  { label: "파일 관리", subLabel: "FILES", path: "/files" },
+  { label: "방송 예약", subLabel: "BOOK", path: "/broadcast" },
+  { label: "방송부", subLabel: "BROAD CASTER", path: "/caster" },
+];
